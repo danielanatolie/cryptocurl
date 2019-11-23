900a68497d5bfd16cead93c39aa97ccc27a30887
Daniel Anatolie <daniel.anatolie@gmail.com>
2019-11-23 10:11:53 -0800

    compute current coin score

--- main.pl
+++ main.pl
@@ -19,41 +19,74 @@ invest(StartingCapital) :-
     write("Your budget is: "),
     write(StartingCapital),
     nl,
-    write("We recommend diversiving into the top 3 coins: BTC, ETH, and XRP"),
-    nl,
-    print_current_prices(),
-    nl.
+    print_current_prices(BTCPrice),
+    (StartingCapital >= BTCPrice ->
+            round(BTCPrice, BTCPriceRounded),
+            write("You'll be able to purchase: "),
+            BTCAmount is div(StartingCapital, BTCPriceRounded),
+            write(BTCAmount),
+            write(" BTC"),
+            nl,
+            computeScore(CoinScore),
+            write("CoinScore is: "),
+            write(CoinScore),
+            % TODO: analyzePriceTrend(PriceTrend),
+            nl
+        ;
+            write("Insufficient funds."),
+            fail
+        ).    
 
-print_current_prices() :-
-    get_current_prices(BTCPrice, ETHPrice, XRPPrice),
+print_current_prices(BTCPrice) :-
+    get_current_prices(BTCPrice),
     write("The current price of: "),
     nl,
     write("Bitcoin (BTC) is: "),
     write(BTCPrice),
-    nl,
-    write("Ethereum (ETH) is: "),
-    write(ETHPrice),
-    nl,
-    write("XRP is: "),
-    write(XRPPrice).
+    nl.
 
-
-get_current_prices(BTCPrice, ETHPrice, XRPPrice) :-
+% ~~~~~~~~~~~~~~~
+% Get BTC prices
+% ~~~~~~~~~~~~~~~
+get_current_prices(BTCPrice) :-
     get_current_price_from_api(Data),
     BTCPriceObj = Data.get(bitcoin),
-    ETHPriceObj = Data.get(ethereum),
-    XRPRiceObj = Data.get(ripple),
-    BTCPrice = BTCPriceObj.get(usd),
-    ETHPrice = ETHPriceObj.get(usd),
-    XRPPrice = XRPRiceObj.get(usd).
+    BTCPrice = BTCPriceObj.get(usd).
 
 get_current_price_from_api(Data) :-
-    URL = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum,ripple,bitcoin&vs_currencies=usd",
+    URL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd",
     setup_call_cleanup(
         http_open(URL, In, []),
         json_read_dict(In, Data),
         close(In)).
 
-% Cryptocurrency Historical Data following:
-% currencyName(allTimeHigh)
-bitcoin(19665).
+% get_last_week_price(BTCPrice, W) :-
+
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~
+% Finds overall coin score
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~
+computeScore(CoinScoreRounded) :-
+    get_coin_data_from_api(Data),
+    DeveloperScore = Data.get(developer_score),
+    CommunityScore = Data.get(community_score),
+    round(DeveloperScore, DeveloperScoreRounded),
+    round(CommunityScore, CommunityScoreRounded),
+    avg([DeveloperScoreRounded, CommunityScoreRounded], CoinScore),
+    round(CoinScore, CoinScoreRounded).
+
+get_coin_data_from_api(Data) :-
+    URL = "https://api.coingecko.com/api/v3/coins/bitcoin",
+    setup_call_cleanup(
+        http_open(URL, In, []),
+        json_read_dict(In, Data),
+        close(In)).
+
+avg(List, Average):- 
+    sum_list(List, Sum),
+    length(List, Length),
+    Length > 0, 
+    Average is Sum / Length.
+
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~
+% Finds overall price trend
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~
